a1ee575cdd62b53225dcd18ebeb29a70c6cd0e08
Alex <ais_alex@ukr.net>
2023-03-10 07:02:54 +0200

    done

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,14 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'components/Button/Button';
 import { statusFilters } from 'redux/constants';
-import { getStatusFilter } from 'redux/selectors';
+import { selectStatusFilter } from 'redux/selectors';
 import { setStatusFilter } from 'redux/filtersSlice';
 import css from './StatusFilter.module.css';
 export const StatusFilter = () => {
   // Получаем ссылку на функцию отправки экшенов
   const dispatch = useDispatch();
 
-  const filter = useSelector(getStatusFilter);
+  const filter = useSelector(selectStatusFilter);
 
   // Вызываем генератор экшена и передаём значение фильтра
   // Отправляем результат - экшен изменения фильтра
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,33 +1,26 @@
 import { useSelector } from 'react-redux';
 import { Task } from 'components/Task/Task';
-import { getTasks, getStatusFilter } from 'redux/selectors';
-import { statusFilters } from 'redux/constants';
+import {
+  // selectTasks,
+  // selectStatusFilter,
+  selectVisibleTasks,
+} from 'redux/selectors';
+// import { statusFilters } from 'redux/constants';
 import css from './TaskList.module.css';
-
-const getVisibleTasks = (tasks, statusFilter) => {
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
-  }
-};
 
 export const TaskList = () => {
   // Получаем массив задач из состояния Redux
-  const tasks = useSelector(getTasks);
+  const tasks = useSelector(selectVisibleTasks);
 
-  // Получаем значение фильтра из состояния Redux
-  const statusFilter = useSelector(getStatusFilter);
+  // // Получаем значение фильтра из состояния Redux
+  // const statusFilter = useSelector(selectStatusFilter);
 
-  // Вычисляем массив задач которые необходимо отображать в интерфейсе
-  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  // // Вычисляем массив задач которые необходимо отображать в интерфейсе
+  // const visibleTasks = getVisibleTasks(tasks, statusFilter);
 
   return (
     <ul className={css.list}>
-      {visibleTasks.map(task => (
+      {tasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
